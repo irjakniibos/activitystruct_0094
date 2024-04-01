5f7a08fa79f0fb81d23199314410ba4ecb092dbc
irjakniibos <irzayaumilsyahrar55@gmail.com>
2024-04-01 15:01:44 +0700

    membuat file struktur yang ke-3

---
+++ StrukturB_3.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat{
+    string desa;
+    string kota;
+
+};
+
+struct Mahasiswa {
+    string nim;
+    string nama;
+    DetailAlamat alamat;
+    string umur;
+};
+
+int main(){
+    Mahasiswa mhs;
+    cout << "Nomor Mahasiswa : " ;
+    getline(cin,mhs.nim);
+    cout << "Nama Mahasiswa : " ;
+    getline(cin,mhs.nama);
+
+    cout << "Alamat Mahasiswa : " <<endl;
+    cout << "\tNama Desa : " ;
+    cin >> mhs.alamat.desa;
+    cout << "\tNama Kota : ";
+    cin >> mhs.alamat.kota;
+
+    cout << "Umur Mahasiswa : ";
+    cin >> mhs.umur;
+
+    cout << endl;
+    cout << "\nNIM : " << mhs.nim;
+    cout << "\nNama : " << mhs.nama;
+    cout << "\nAlamat : ";
+    cout << "\n\tDesa : " << mhs.alamat.desa;
+    cout << "\n\tKota : " << mhs.alamat.kota;
+    cout << "\nUmur : " << mhs.umur;
+
+
+}
