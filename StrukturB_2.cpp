99e52bd25a9315ad769239b5044f6f66ccd29f42
irjakniibos <irzayaumilsyahrar55@gmail.com>
2024-04-01 14:44:39 +0700

    membuat prosedur utama

--- StrukturB_2.cpp
+++ StrukturB_2.cpp
@@ -14,3 +14,28 @@ struct Mahasiswa {
     string umur;
 };
 
+int main(){
+    Mahasiswa mhs;
+    cout << "Nomor Mahasiswa : ";
+    cin >> mhs.nim;
+    cout << "Nama Mahasiswa : ";
+    cin >> mhs.nama; 
+
+    cout << "Alamat Mahasiswa : " <<endl;
+    cout << "\tNama Desa : " ;
+    cin >> mhs.alamat.desa;
+    cout << "\tNama Kota : ";
+    cin >> mhs.alamat.kota;
+
+    cout << "Umur Mahasiswa : ";
+    cin >> mhs.umur;
+
+    cout << endl;
+    cout << "\nNIM : " << mhs.nim;
+    cout << "\nNama : " << mhs.nama;
+    cout << "\nAlamat : ";
+    cout << "\n\tDesa : " << mhs.alamat.desa;
+    cout << "\n\tKota : " << mhs.alamat.kota;
+    cout << "\nUmur : " << mhs.umur;
+
+}
